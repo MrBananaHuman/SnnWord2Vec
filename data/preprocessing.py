d7b28036e8775344277380370f09d9acaacf7ef0
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2019-10-25 12:28:56 +0900

    Update preprocessing.py

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,28 +1,56 @@
-vocab = open('vocab.txt', 'r', encoding='utf-8')
-corpus = open('corpus.txt', 'r', encoding='utf-8')
-output = open('preprocessed_corpus.txt', 'w', encoding='utf-8')
+// Seonghyun Kim
 
-vocab_lines = vocab.readlines()
-vocab.close()
+v_init = 0
 
-vocab_id = dict()
-id = 0
+load_file("nrngui.hoc")
+load_file("IafCell.hoc")
+xopen("modeling.hoc")
 
-for line in vocab_lines:
-    line = line.replace('\n', '')
-    vocab_id[line] = id
-    id += 1
+cvode_active(0)
 
-corpus_lines = corpus.readlines()
-corpus.close()
+proc init(){
+	finitialize()
+	if (cvode.active()) {
+		cvode.re_init()
+	} else {
+		fcurrent()
+	}
+	frecord_init()
+}
 
-for line in corpus_lines:
-    line = line.replace('\n', '')
-    output_line = ''
-    for word in line.split(' '):
-        if word in vocab_id:
-            output_line += vocab_id[word] + ' '
-    output.write(output_line.strip() + '\n')
+simulation_time = 1000
+tstop = simulation_time
 
-output.close()
+objref corpus_file
 
+corpus_file = new File()
+corpus_file.ropen("../data/corpus.txt")
+
+corpus_sent_num  = 0
+
+strdef current_sent
+while(!corpus_file.eof()){
+    corpus_file.gets(current_sent)
+	corpus_sent_num += 1
+}
+
+
+objref sentences[corpus_sent_num]
+for(i = 0; i < corpus_sent_num; i += 1){
+    sentences[i] = new Vecotr()
+}
+
+corpus_file.ropen("../data/corpus.txt")
+sent_idx = 0
+data_idx = 0
+objref ch
+while(!corpus_file.eof()){
+    ch = corpus_file.scanvar()
+    sentences[sent_dix].insrt(data_idx, corpus_file.scanvar())
+    if(ch == 0){
+        sent_idx += 1
+        data_idx = 0
+    } else{
+        data_idx = 0
+    }
+}
